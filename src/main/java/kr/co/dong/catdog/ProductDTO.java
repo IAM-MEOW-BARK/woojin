b0d99c93a9c6b0218cc3f0720e72ce72877c2899
402-20 <402-20@DESKTOP-SA31OOH>
2024-11-22 13:21:36 +0900

    product dto 등록

--- src/main/java/kr/co/dong/catdog/ProductDTO.java
+++ src/main/java/kr/co/dong/catdog/ProductDTO.java
@@ -1,39 +1,76 @@
 package kr.co.dong.catdog;
 
 public class ProductDTO {
-	private int product_id;
-	private int product_group_id;
-	private String product_name;
-	private int product_price;
-	
-	public int getProduct_id() {
-		return product_id;
-	}
-	public void setProduct_id(int product_id) {
-		this.product_id = product_id;
-	}
-	public int getProduct_group_id() {
-		return product_group_id;
-	}
-	public void setProduct_group_id(int product_group_id) {
-		this.product_group_id = product_group_id;
-	}
-	public String getProduct_name() {
-		return product_name;
-	}
-	public void setProduct_name(String product_name) {
-		this.product_name = product_name;
-	}
-	public int getProduct_price() {
-		return product_price;
-	}
-	public void setProduct_price(int product_price) {
-		this.product_price = product_price;
-	}
-	
-	@Override
-	public String toString() {
-		return "ProductDTO [product_id=" + product_id + ", product_group_id=" + product_group_id + ", product_name="
-				+ product_name + ", product_price=" + product_price + "]";
-	}
+   private int product_code;
+   private String product_name;
+   private int product_category;
+   private int product_price;
+   private String thumbnail_img;
+   private String product_img;
+   private String product_info;
+   private String product_regdate;
+   private String product_update;
+   public int getProduct_code() {
+      return product_code;
+   }
+   public void setProduct_code(int product_code) {
+      this.product_code = product_code;
+   }
+   public String getProduct_name() {
+      return product_name;
+   }
+   public void setProduct_name(String product_name) {
+      this.product_name = product_name;
+   }
+   public int getProduct_category() {
+      return product_category;
+   }
+   public void setProduct_category(int product_category) {
+      this.product_category = product_category;
+   }
+   public int getProduct_price() {
+      return product_price;
+   }
+   public void setProduct_price(int product_price) {
+      this.product_price = product_price;
+   }
+   public String getThumbnail_img() {
+      return thumbnail_img;
+   }
+   public void setThumbnail_img(String thumbnail_img) {
+      this.thumbnail_img = thumbnail_img;
+   }
+   public String getProduct_img() {
+      return product_img;
+   }
+   public void setProduct_img(String product_img) {
+      this.product_img = product_img;
+   }
+   public String getProduct_info() {
+      return product_info;
+   }
+   public void setProduct_info(String product_info) {
+      this.product_info = product_info;
+   }
+   public String getProduct_regdate() {
+      return product_regdate;
+   }
+   public void setProduct_regdate(String product_regdate) {
+      this.product_regdate = product_regdate;
+   }
+   public String getProduct_update() {
+      return product_update;
+   }
+   public void setProduct_update(String product_update) {
+      this.product_update = product_update;
+   }
+   @Override
+   public String toString() {
+      return "ProductDTO [product_code=" + product_code + ", product_name=" + product_name + ", product_category="
+            + product_category + ", product_price=" + product_price + ", thumbnail_img=" + thumbnail_img
+            + ", product_img=" + product_img + ", product_info=" + product_info + ", product_regdate="
+            + product_regdate + ", product_update=" + product_update + "]";
+   }
+
+   
 }
